a6ad6eb59f0950773855886618a0d30871bb3347
Elvin Ciqueira <elvinciqueira@gmail.com>
2022-08-07 14:48:45 -0300

    behavioral strategy pattern

---
+++ src/behavioral/strategy/AirportTicketCalcutalor.ts
@@ -0,0 +1,9 @@
+import Period from './Period';
+import TicketCalculator from './TicketCalculator';
+
+export default class AirportTicketCalculator implements TicketCalculator {
+  calculate(period: Period): number {
+    const price = 30;
+    return price;
+  }
+}
---
+++ src/behavioral/strategy/TicketCalculatorFactory.ts
@@ -0,0 +1,17 @@
+import AirportTicketCalculator from './AirportTicketCalcutalor';
+import BeachTicketCalculator from './BeachTicketCalculator';
+import ShoppingTicketCalculator from './ShoppingTicketCalculator';
+
+export default class TicketCalculatorFactory {
+  static create(location: string) {
+    if (location === 'beach') {
+      return new BeachTicketCalculator();
+    }
+    if (location === 'shopping') {
+      return new ShoppingTicketCalculator();
+    }
+    if (location === 'airport') {
+      return new AirportTicketCalculator();
+    }
+  }
+}
